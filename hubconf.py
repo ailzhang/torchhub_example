9fdef241889e3fbc7e04513b4e8d408106c8f534
Ailing Zhang <ailzhang@fb.com>
2019-09-09 22:30:30 -0700

    add url

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,9 @@
 dependencies = ['torch']
+import torch
 from model import Net
 
-def mnist():
+def mnist(pretrained=False):
     m = Net()
+    if pretrained:
+        m.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/ailzhang/torchhub_example/releases/download/0.1/mnist_init_ones'))
     return m
