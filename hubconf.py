d4388bd5c2840adc8ddbc7b8d8335118abed68bf
Ailing Zhang <ailzhang@fb.com>
2019-09-10 21:14:36 -0700

    add tar&zip

--- hubconf.py
+++ hubconf.py
@@ -7,3 +7,16 @@ def mnist(pretrained=False):
     if pretrained:
         m.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/ailzhang/torchhub_example/releases/download/0.1/mnist_init_ones'))
     return m
+
+def mnist_tar(pretrained=False):
+    m = Net()
+    if pretrained:
+        m.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/ailzhang/torchhub_example/releases/download/0.1/mnist_init_ones.tar.gz'))
+    return m
+
+def mnist_zip(pretrained=False):
+    m = Net()
+    if pretrained:
+        m.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/ailzhang/torchhub_example/releases/download/0.1/mnist_init_ones.zip'))
+    return m
+
