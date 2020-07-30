e41c88a70e838cfef38adc702848906d55616306
Ailing Zhang <ailzhang@fb.com>
2020-07-30 11:41:43 -0700

    Add a new example produced by 1.6 release.

--- hubconf.py
+++ hubconf.py
@@ -20,3 +20,9 @@ def mnist_zip(pretrained=False):
         m.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/ailzhang/torchhub_example/releases/download/0.1/mnist_init_ones.zip'))
     return m
 
+def mnist_zip_1_6(pretrained=False):
+    m = Net()
+    if pretrained:
+        m.load_state_dict(torch.hub.load_state_dict_from_url('https://github.com/ailzhang/torchhub_example/releases/download/0.1/mnist_init_ones_1_6.zip'))
+    return m
+
